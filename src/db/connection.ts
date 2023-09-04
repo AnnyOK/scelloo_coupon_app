f0f465dda492d0e73721c1a4643d917e69326687
AnnyOK <okpalaanayo@gmail.com>
2023-09-04 21:16:23 +0100

    commit for deploy

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -7,7 +7,7 @@ import { User } from "../models/user";
 import { Cart } from "../models/carts";
 const connection = new Sequelize({
   dialect:"postgres",
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
